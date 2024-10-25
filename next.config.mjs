8c3a2c8b6cf16a779e8fcef2710a8c0373edf7c8
jecu13 <jeffrey@bluehive.com.ph>
2024-10-25 16:29:14 +0800

    fix scanning error being triggered even when not focusing on qr code

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    // Enable Webpack 5
+    config.experiments = { ...config.experiments, topLevelAwait: true };
+
+    // Optional: Adjust other Webpack settings for better debugging
+    if (!isServer) {
+      config.resolve.fallback = { fs: false, net: false, tls: false };
+    }
+
+    return config;
+  },
+  reactStrictMode: true, // Helps with debugging React-related issues
+  productionBrowserSourceMaps: true, // Enables source maps in production to help with debugging
+};
 
 export default nextConfig;
